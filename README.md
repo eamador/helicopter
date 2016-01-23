1787f833bb57c4b645a2759afd9c88323540f0af
Eduardo <eduardo.amador@cocomore.com>
2016-01-23 19:41:06 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Helicopter iPhone 6s
+
+based on https://github.com/pltarun/scale
+based on https://github.com/freinbichler/3d-touch
---
+++ helicopter.js
@@ -0,0 +1,69 @@
+// based on https://github.com/freinbichler/3d-touch
+var touchElement = document.getElementById('forcearea');
+var forceTextElement = document.getElementById('peso');
+var helicopterElement = document.getElementById('helicopter');
+var forceValue = 0;
+var tareValue = 0;
+
+var screenHeight = window.innerHeight
+|| document.documentElement.clientHeight
+|| document.body.clientHeight;
+
+function onTouchStartMove(e) {
+  e.preventDefault();
+  checkForce(e);
+}
+
+function onTouchEnd(e) {
+  e.preventDefault();
+  touch = null;
+}
+
+function checkForce(e) {
+  if (e.target.id == "buttonarea" || e.target.id == "tareButton") {
+    tare();
+  } else {
+    touch = e.touches[0];
+    setTimeout(refreshforceValue.bind(touch), 10);
+  }
+}
+
+function checkMacForce(e) {
+  // max value for trackpad is 3.0 compare to 1.0 on iOS
+  forceValue = e.webkitForce / 3;
+  // forceTextElement.innerHTML = ((forceValue - tareValue) * 1000).toFixed(2) + " <br><span style=\"color:red\">Uncalibrated for Mac, press \"t\" to tare.</span>";
+}
+
+function refreshforceValue() {
+  var touchEvent = this;
+
+  forceValue = 0;
+  if (touchEvent) {
+    forceValue = touchEvent.force || 0;
+    setTimeout(refreshforceValue.bind(touch), 10);
+  }
+  //Remove the tareValue from the returned value
+  var up = screenHeight / 385;
+
+  helicopterElement.style.top = ((forceValue - tareValue) * 385).toFixed(4) * up;
+  forceTextElement.innerHTML = ((forceValue - tareValue) * 385).toFixed(2) + " grams";
+}
+
+function keypress(e) {
+  if (e.keyCode == 116) {
+    tare();
+  }
+}
+
+function tare() {
+  tareValue = forceValue;
+}
+
+(function() {
+  touchElement.addEventListener('touchstart', onTouchStartMove, false);
+  touchElement.addEventListener('touchmove', onTouchStartMove, false);
+  touchElement.addEventListener('touchend', onTouchEnd, false);
+  touchElement.addEventListener('webkitmouseforcewillbegin', checkMacForce, false);
+  touchElement.addEventListener('webkitmouseforcechanged', checkMacForce, false);
+  document.addEventListener('keypress', keypress, false)
+})();
